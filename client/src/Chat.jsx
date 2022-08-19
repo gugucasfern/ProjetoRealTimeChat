4dfa1051ff889171596a5376a29a570ca73b4385
gustavohirose <gustavohirose@HCF3339.FAMEMA>
2022-08-19 16:56:59 -0300

    inicio da adicao do estilo

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+import { Container } from 'shards-react';
 const client = new ApolloClient({
     uri: 'http://localhost:4000/',
     cache: new InMemoryCache(),
   });
 
 
-  const Chat = () =>{
+const GET_MESSAGES = gql`
+query {
+  messages {
+    id
+    content
+    user 
+  }
+}`;
+
+const Messages = ({ user }) => {
+    const { data } = useQuery(GET_MESSAGES);
+    if (!data){
+        return null
+    }
     return (
-        <div> Salve sou o chat</div>
-    );
+        <>
+        #começo da adição do estilo
+            {data.messages.map(({id, user , content})=>
+                <div></div>
+            )}
+        </>
+    )
+
+}
+
+  const Chat = () =>{
+    return <Container> <Messages user="Jao" /></Container>
   };
 
   export default () =>(
